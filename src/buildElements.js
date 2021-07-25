457159e0eadafdc948f6ab1126b39a177b98d2db
namdlefhtes <namdlefhtes@gmail.com>
2021-07-25 11:20:24 -0400

    working element factory

--- src/buildElements.js
+++ src/buildElements.js
@@ -1,19 +1,32 @@
-const elFactory = (type, parent/*attributes, child*/) => {
+const elFactory = (type, parent, title/*attributes, child*/) => {
+    const build = () => {
+        let el = document.createElement(type);
+        let appendAble = document.getElementById(parent);
+        el.id = title;
+        el.innerText = "testesttest";
+        appendAble.append(el);
+        console.log(type, parent + "awesae");
+    };
+
     return {
-        type: type,
-        parent: parent
+        type,
+        parent,
+        title,
+        build
+    }
+
     };
 
     // let element = document.createElement(type);
     // let parent = document.getElementsByName(parent);
     // document.body.append(element);
     // return element ;
-};
 
-let firstEl = elFactory('div', 'body');
-console.log(firstEl.type);
+const firstEl = elFactory('div', 'main', "div1");
+const secondEl = elFactory("h2", 'div1', "test2" );
 
-
+firstEl.build();
+secondEl.build();
 
 
 export {elFactory}
