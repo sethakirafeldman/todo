aa461e426fa8c1ecdf2536bae45640fccd4831b3
namdlefhtes <namdlefhtes@gmail.com>
2021-08-06 09:25:51 -0400

    cleaned up buildTaskForm

--- src/buildTask.js
+++ src/buildTask.js
@@ -1,37 +1,16 @@
 import { submitData } from "./submitData";
 
-const multiAttr = [
-    ['type', 'text'],
-    ['task', 'Task Name'],
-    ['placeholder', 'Task Name'],
-    ['type', 'value'],
-    ['size', '10']
-];
-//multi dimensional array loop is whacky...
-let attributeSetter = (element) => {
-    for ( let i=0; i < multiAttr.length[i]; i++) {
-        for ( let j=0; j < multiAttr[i][j].length; j++) {
-            console.log(i + j + "i and J values");
-            // element.setAttribute(multiAttr[i][j], multiAttr[i][j]);
-        };
+const attributeSetter = (element, attributes) => {
+    for ( let key in attributes) {
+        element.setAttribute(key, attributes[key]);
     };
 };
 
 const buildTaskForm = () => {
-
-    
-    // code quite repetitive. create a factory func or something to
-    // addd these attributes.
-
     // hide Add Task button
     document.getElementById("addTask").classList.add("hide");
     
     let container = document.getElementById("container");
-    
-
-    // console.log(multiAttr.length);
-    
-
 
     // create inputContainer
     let inputContainer = document.createElement("div");
@@ -39,33 +18,38 @@ const buildTaskForm = () => {
     container.appendChild(inputContainer);
     
     //create taskName input
-    let taskInput = document.createElement("input");
-    // attributeSetter(taskInput);
-
-    taskInput.id = "taskInput";
-    taskInput.setAttribute("type", "text");
-    taskInput.setAttribute("task", "Task Name")
-    taskInput.setAttribute("placeholder", "Task Name");
-    taskInput.setAttribute("type", "value");
-    taskInput.setAttribute("size","10")
+    const taskInput = document.createElement("input");
+    attributeSetter(taskInput, {
+        "id": "taskInput",
+        "type": "text",
+        "task": "Task Name",
+        "placeholder": "Task Name",
+        "type": "value",
+        "size": "10"
+    });
     inputContainer.appendChild(taskInput);
     
     //create taskDescription input
-    let taskDescription = document.createElement("input");
-    taskDescription.id = "taskDescription";
-    taskDescription.setAttribute("type","text");
-    taskDescription.setAttribute("task","Task Description");
-    taskDescription.setAttribute("placeholder", "Description");
-    taskDescription.setAttribute("type","value");
-    taskDescription.setAttribute("size","50");
+    const taskDescription = document.createElement("input");
+    attributeSetter(taskDescription, {
+        "id": "taskDescription",
+        "type": "text",
+        "task": "Task Description",
+        "placeholder": "Description",
+        "type": "value",
+        "size": "50"
+    });
+
     inputContainer.appendChild(taskDescription);
     
     // create submit button
-    let submit = document.createElement("input");
-    submit.setAttribute("type","submit");
-    submit.setAttribute("value", "Submit");
+    const submit = document.createElement("input");
+    attributeSetter(submit, {
+        "type": "submit",
+        "value": "Submit",
+        "id": "submitBtn"
+    });
     submit.addEventListener("click", submitData);
-    submit.id= "submitBtn";
     inputContainer.appendChild(submit);    
 
 };
