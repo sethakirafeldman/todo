6aab136a3c89f3a9861b3a9f7ca2b7580b0e0c78
namdlefhtes <namdlefhtes@gmail.com>
2021-08-17 17:13:05 -0400

    making prog on listing projects

--- src/checkProjects.js
+++ src/checkProjects.js
@@ -6,31 +6,51 @@ let activeProjects = [];
 
 const checkProjects = ()=> {
     let task = "task_";
+    let temp = "";
+    let temp2 = "";
+    let temp3 = "";
+    let projName = "";
     for (let i =1; i < 1000; i++) {
-            if (window[task+i] === undefined) {
-                console.log("undefined, stop");
-                return undefined;
-            }
+        //trying to pass in the name of the project to test if element with same name already exists in DOM.
+        projName = window[task+i].project; 
 
-            else  {
-                
-                for (let j =0; j < 100; j++ ) {
-                   if (window[task+j] !== undefined) {
-                       console.log(window[task+j].project);
-                       let projEl = document.createElement("div");
-                       projEl.innerHTML= window[task+j].project;
-                       document.getElementById("Project").appendChild(projEl);
+        console.log(projName+" testingprojName");
 
-                       // this logic needs to cap only unique project names
-                   }
+        // not sure what this even does.
+        if (window[task+i] === undefined) {
+            console.log("undefined, stop");
+            return undefined;
+        }
 
-                   else {
+        else if (document.body.contains(document.getElementById(projName) ) === true) {
+            console.log("EXISTSS");
+            // part of this is failing possibly.
+        }
 
-                   }
-               
-                };
+        else { 
+            
+            // add name of project to sidebar
+            temp = window[task+i];
+            let projEl = document.createElement("div");
+            projEl.innerHTML= window[temp.project].projectName;
+            projEl.id = window[temp.project].projectName;
+            temp2 = window[temp.project].tasks;
+            document.getElementById("Project").appendChild(projEl);
+
+            // list out tasks that are part of project
+            let li =document.createElement("li");
+            li.innerHTML = temp2;
+            projEl.appendChild(li);
+
+            temp = "";
+            temp2 = "";
+            // need to grab by project object name as these now storing tasks.
             };
-    } 
+        
+            // }
+    };
+
 };
 
-export { checkProjects }
+
+export { checkProjects}
