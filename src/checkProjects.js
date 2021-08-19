cf97b123a0c9e0a4ac2e1bbaa184ff6d22f3bfdf
namdlefhtes <namdlefhtes@gmail.com>
2021-08-19 08:36:25 -0400

    checkprojects pre refactor

--- src/checkProjects.js
+++ src/checkProjects.js
@@ -8,48 +8,64 @@ const checkProjects = ()=> {
     let task = "task_";
     let temp = "";
     let temp2 = "";
-    let temp3 = "";
     let projName = "";
+    let projTasks = "";
+
 
     for (let i =1; i < 1000; i++) {
-        //trying to pass in the name of the project to test if element with same name already exists in DOM.
         projName = window[task+i]; 
-
-        console.log(projName+" testingprojName");
 
         // not sure what this even does.
         if (window[task+i] === undefined) {
-            console.log("undefined, stop");
             return undefined;
         }
 
-        else if (document.body.contains(document.getElementById(projName.project) ) === true) {
-            console.log("EXISTSS");
-            let li = document.createElement("li");
-            li.id = projName;
-            document.getElementById(projName.project).appendChild(li);
+        else { 
+        // if project with name already exists.
+            for (let j = 0 ; j < window[projName.project].tasks.length; j++ ) {
+                projTasks = window[projName.project].tasks[j];
+
+                if (document.body.contains(document.getElementById(projName.project) ) === true) {
+                //     //trying to remove list items before looping through to re-add.
+                    document.getElementById(projName.project).remove();
         
+                    let projEl = document.createElement("div");
+                    projEl.innerHTML= window[projName.project].projectName;
+        
+                    projEl.id = window[projName.project].projectName;
+                    document.getElementById("Project").appendChild(projEl);
+        
+                    // list out tasks that are part of project
+                    let li =document.createElement("li");
+                    li.id = projTasks;
+                    li.innerHTML = projTasks;
+                    projEl.appendChild(li);
+                }
+                // adds new project with first task to DOM.
+                else { 
+        
+                    let projEl = document.createElement("div");
+                    projEl.innerHTML= window[projName.project].projectName;
+        
+                    projEl.id = window[projName.project].projectName;
+                    // projTasks = window[projName.project].tasks[0];
+                    document.getElementById("Project").appendChild(projEl);
+        
+                    // list out tasks that are part of project
+                    let li = document.createElement("li");
+                    li.id = projTasks;
+                    li.innerHTML = projTasks;
+                    projEl.appendChild(li);
+        
+                    temp = "";
+                    temp2 = "";
+        
+                    };
+            }
         }
 
-        else { 
-            
-            // add name of project to sidebar
-            temp = window[task+i];
-            let projEl = document.createElement("div");
-            projEl.innerHTML= window[temp.project].projectName;
-            projEl.id = window[temp.project].projectName;
-            temp2 = window[temp.project].tasks;
-            document.getElementById("Project").appendChild(projEl);
 
-            // list out tasks that are part of project
-            let li =document.createElement("li");
-            li.innerHTML = temp2;
-            projEl.appendChild(li);
 
-            temp = "";
-            temp2 = "";
-            // need to grab by project object name as these now storing tasks.
-            };
         
             // }
     };
