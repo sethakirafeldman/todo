5ccea8b659708dfe86e49724367719d8b6af3b96
namdlefhtes <namdlefhtes@gmail.com>
2021-09-14 08:32:39 -0700

    working delete

--- src/deleteObject.js
+++ src/deleteObject.js
@@ -1,36 +1,34 @@
 // need to figure out better algo. too confusing. 
 
 const deleteObj = (task) => {
-    let deleteTask = window[task.id].entry;
-    let checkTask = window[`task_${deleteTask}`];
-
-        //sets task object is undefined + should set project object to undefined if no tasks exist.
-        Object.values(activeProjects).forEach(val => {
+    let tNum = window[task.id].entry;
+    let checkTask = window[`task_${tNum}`];
+        //looks through projects for matching task.
+        Object.values(activeProjects).forEach(AP => {
             // checks activeProjects for when projectName matches project name of task obj.
-            if (val.projectName === checkTask.project) {
-
-                    // loops through active projects [p]
-                    Object.values(activeProjects).forEach(p => {
-                        console.log(checkTask.title);
-                        // loops through each project (p)
-                        p.forEach(q => {
-                          if (p.tasks[q].title === task.title) {
-                              console.log("task is"+ p.tasks[q].title);
-                          }   
-                          
-                        //sets task object to undefined.
-
-                        //   window[`task_${deleteTask}`] = undefined;
-
-                        })
-                        // console.log(p.tasks);
-                        // p.tasks points to tasks within project. 
-                        
-                    
-
-                    })
-            }
-    });
+            if (AP.projectName === checkTask.project) {
+                // check if checkTask.title in AP.tasks[0];
+                for (let i=0; i < AP.tasks.length; i++) {
+                    // marks task as deleted.
+                    if (AP.tasks[i].title === checkTask.title) {  
+                        ( () => {
+                            if (confirm("Are you sure you want to delete this task?")){
+                            
+                                // AP.tasks[i] = undefined;   
+                                //deletes tasks from DOM.
+                                document.getElementById(`task_${tNum}`).remove();
+                                document.getElementById(`${checkTask.title}`).remove();
+                                //sets task status to deleted
+                                checkTask.status = "deleted";
+                                document.getElementById("editContainer").remove();
+                            }
+                            else {
+                            } 
+                        })();
+                    };
+                };            
+            };
+        });
 }
 
 
