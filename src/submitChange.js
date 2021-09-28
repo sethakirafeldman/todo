3ece36971304c50813870b7fbf0ccdd5de129de4
namdlefhtes <namdlefhtes@gmail.com>
2021-09-28 09:26:49 -0400

    working editor now.

--- src/submitChange.js
+++ src/submitChange.js
@@ -2,52 +2,55 @@ import { genSideBarContent } from "./genSideBarContent";
 
 const submitChange = (taskObj) => {
     localStorage.clear() // temp
-
+    //targets selected DOM task to edit
     const taskObjEl = document.getElementById(taskObj);
-    
+    //Additional DOM elements to edit.
     const taskEditBox = document.getElementById("taskEdit");
     const descEditBox = document.getElementById("descEdit");
     const dateEditBox = document.getElementById("dateEdit");
 
-    //checks if any value in editor contains value AKA not blank.
+    //checks if any value in editor contains contains a value.
     if (taskEditBox.value !== "" || descEditBox.value !== "" || dateEditBox.value !== "") {
         // console.log("values: "+ taskEditBox.value + descEditBox.value + dateEditBox.value);
+        // stores values from editor in object.
+        let tempArr = [taskEditBox.value, descEditBox.value, dateEditBox.value];
+         
+        console.log(tempArr+ "values??!");
         let tempVals = {
             taskEdit: taskEditBox.value, 
             descEdit: descEditBox.value,
             dateEdit: dateEditBox.value 
         };
-            for (let i = 0; i < 3; i++) {
-                let checkVal = Object.values(tempVals)[i]; 
-                let checkKey = Object.keys(tempVals)[i];
-                    if (checkKey == "taskEdit" && checkVal !== "") {
-                        window[taskObj].title = checkVal;
-                    }
-                    else if (checkKey == "descEdit" && checkVal !== "") {
-                        window[taskObj.description] = checkVal;
-                    }
-                    else if (checkKey == "dateEdit" && checkVal !== "" ) {
-                        window[taskObj.dueDate] = checkVal;
-                    }
-                    else {}
-            };
-            
+        // modify task_# obj only if there is a value in the editor box
+        for (let i=0; i < 3; i++) {
+            // checks what vals in editor are blank.
+            if (Object.values(tempVals)[i] == "") {
+                console.log(Object.keys(tempVals)[i] +"is blank");
+            }
+            // checks what vals in editor have value.
+            else if (Object.values(tempVals)[i] !== "") {
+                console.log(Object.keys(tempVals)[i]+ "has a value");
+                //changes task Object based on which has value.
+                if (Object.keys(tempVals)[i] == "taskEdit"){
+                    window[taskObj].title = Object.values(tempVals)[i];
+                }
+                else if (Object.keys(tempVals)[i] == "descEdit") {
+                    window[taskObj].description = Object.values(tempVals)[i];                    
+                }
+                else if (Object.keys(tempVals)[i] == "dateEdit") {
+                    window[taskObj].dueDate = Object.values(tempVals)[i];
+                }
+            }
+        }      
     }
    
     else {
         // no change to obj/DOM.
     }    
-    // this code works except it needs to avoid overwriting with blank entries if no value is entered. 
-    // window[taskObj].title = taskEditBox.value;
-    // window[taskObj].description = descEditBox.value;
-    // window[taskObj].dueDate = dateEditBox.value;
-
-    // taskObjEl.childNodes[0].value = window[taskObj].title;
-
+    // writes task data to DOM based on what is in obj.
     taskObjEl.getElementsByClassName("taskTitle")[0].innerText = window[taskObj].title;  
     taskObjEl.getElementsByClassName("taskContent")[0].innerText = window[taskObj].description;  
     taskObjEl.getElementsByClassName("taskDueDate")[0].innerText = window[taskObj].dueDate;  
-
 
     genSideBarContent();
 
