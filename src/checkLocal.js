96d1c822c9a3922e067ce4a1f7d19154f3c25f5e
namdlefhtes <namdlefhtes@gmail.com>
2021-10-02 10:00:38 -0400

    render cancels if tasks marked as deleted

--- src/checkLocal.js
+++ src/checkLocal.js
@@ -7,17 +7,23 @@ const checkLocal = () => {
     if (localStorage.length > 0) {
         console.log("existing projects... loading");
         let tNum = 1;
-
+        /// this needs to check all tasks in projects to see if they contain the deleted status to prevent loading wrong ones. 
         for (let i= 1; i <= localStorage.length; i++) {
             let localProject = JSON.parse(localStorage.getItem(`project_${i}`));
             activeProjects[`project_${i}`] = new Object(localProject);
             
             // checks for all tasks in given project.
+        
             for (let j = 0; j < localProject.tasks.length; j++) {
-                window[`task_${tNum}`] = localProject.tasks[j];
-                genPageContent(`task_${tNum}`);
-                genSideBarContent();
-                taskNumber++;
+                if (localProject.tasks[j].status !== "deleted") {
+                    window[`task_${tNum}`] = localProject.tasks[j];
+                    genPageContent(`task_${tNum}`);
+                    genSideBarContent();
+                    taskNumber++;
+                }
+                else {
+                    console.log("tasks have been deleted, do not render");
+                }
             }
 
         }   
