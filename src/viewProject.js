9280b9ef97b4b2f812ad579423a70c8ffd391827
namdlefhtes <namdlefhtes@gmail.com>
2021-10-04 09:11:33 -0400

    data renders to window

--- src/viewProject.js
+++ src/viewProject.js
@@ -2,6 +2,7 @@
 const viewProject = (projName) => {
     // remove current elements from dom
     let taskSec = document.getElementById("taskSection");
+    let actProj = activeProjects[projName];
     taskSec.innerHTML = "";
     
     // render project details
@@ -9,13 +10,30 @@ const viewProject = (projName) => {
     // render project title
     let title = document.createElement("div");
     title.id = `${projName}_title`;
-    title.innerHTML = activeProjects[projName].projectName;
+    title.innerHTML = actProj.projectName;
     taskSec.appendChild(title);
 
     //render project tasks (loop)
-
+    for (let i = 0; i < actProj.tasks.length; i++ ){
+        let li = document.createElement("li");
+        li.id = `viewTask_${actProj.tasks[i].title}`
+        li.innerHTML = actProj.tasks[i].title;
+        title.appendChild(li);
+            for (let j=1; j < Object.values(actProj.tasks[i]).length; j++) {
+                if (Object.keys(actProj.tasks[i])[j] == "entry") {
+                    // skips entry.
+                }
+                else {    
+                    console.log(Object.values(actProj.tasks[i])[j]);
+                    let li2 = document.createElement("li");
+                    li2.innerHTML = `${Object.values(actProj.tasks[i])[j]}`;
+                    li.appendChild(li2);
+                }
+            };
+    };
     
+}    
    
-}
+
 
 export{ viewProject}
