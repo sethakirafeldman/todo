98212f5e3b01a83c294e74d0e04112d2f227b172
namdlefhtes <namdlefhtes@gmail.com>
2021-10-08 09:05:34 -0400

    edits store to localstorage

--- src/submitChange.js
+++ src/submitChange.js
@@ -14,7 +14,7 @@ const submitChange = (taskObj) => {
     if (taskEditBox.value !== "" || descEditBox.value !== "" || dateEditBox.value !== "") {
         // console.log("values: "+ taskEditBox.value + descEditBox.value + dateEditBox.value);
         // stores values from editor in object.
-        let tempArr = [taskEditBox.value, descEditBox.value, dateEditBox.value];
+        // let tempArr = [taskEditBox.value, descEditBox.value, dateEditBox.value];
          
         let tempVals = {
             taskEdit: taskEditBox.value, 
@@ -38,8 +38,7 @@ const submitChange = (taskObj) => {
                 else if (Object.keys(tempVals)[i] == "dateEdit") {
                     window[taskObj].dueDate = Object.values(tempVals)[i];
                 }
-                // trying to store newly changed values in task obj.
-                // localStorage.setItem(window["task_1"], JSON.stringify(taskObj));
+         
             }
         }      
     }
@@ -47,15 +46,24 @@ const submitChange = (taskObj) => {
     else {
         // no change to obj/DOM.
     }    
-    // writes task data to DOM based on what is in obj.
-    taskObjEl.getElementsByClassName("taskTitle")[0].innerText = window[taskObj].title;  
-    taskObjEl.getElementsByClassName("taskContent")[0].innerText = window[taskObj].description;  
-    taskObjEl.getElementsByClassName("taskDueDate")[0].innerText = window[taskObj].dueDate;  
 
-    genSideBarContent();
+// writes task data to DOM based on what is in obj.
+taskObjEl.getElementsByClassName("taskTitle")[0].innerText = window[taskObj].title;  
+taskObjEl.getElementsByClassName("taskContent")[0].innerText = window[taskObj].description;  
+taskObjEl.getElementsByClassName("taskDueDate")[0].innerText = window[taskObj].dueDate;  
 
-    document.getElementById("editContainer").remove();
+    //stores changes made in edit to local storage.
+    for (let j=0; j < Object.values(activeProjects).length; j++) {
+        let projectValues = Object.values(activeProjects)[j];
+       
+        if ( projectValues.projectName == window[taskObj].project) {
+            localStorage.setItem(Object.keys(activeProjects)[j], JSON.stringify(projectValues));
+        }
+    };       
 
+
+genSideBarContent();
+document.getElementById("editContainer").remove();
 }
 
 export {submitChange}
